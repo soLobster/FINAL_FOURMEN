239876527293d640457afb38f01779245f74d194
soLobster <dhwnd0526@gmail.com>
2024-01-24 17:31:16 +0900

    koj commit

---
+++ TeamFourmen/src/main/java/com/itwill/teamfourmen/dto/MovieListDTO.java
@@ -0,0 +1,10 @@
+package com.itwill.teamfourmen.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MovieListDTO {
+	private List<MovieDTO> results;
+}
--- TeamFourmen/src/main/java/com/itwill/teamfourmen/web/HomeController.java
+++ TeamFourmen/src/main/java/com/itwill/teamfourmen/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.itwill.teamfourmen.web;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,11 +11,20 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class HomeController {
 
+	@Value("${api.themoviedb.api-key}")
+	private String API_KEY;
+	
 	@GetMapping("/")
 	public String home() {
 		log.info("HOME()");
 		
-		return "index";
+		
+		int movieId = 609681;
+		
+		
+		return "redirect:https://api.themoviedb.org/3/movie/" + movieId+ "?api_key="+ API_KEY;
 	}
+	
+
 	
 }
