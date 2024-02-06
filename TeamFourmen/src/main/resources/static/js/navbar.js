fc84c447e6a9027dbdbd2b7a6c9456a3061bdea4
Hwi Dong Park <cirche1@naver.com>
2024-02-06 19:58:21 +0900

    work on navbar

--- TeamFourmen/src/main/resources/static/js/navbar.js
+++ TeamFourmen/src/main/resources/static/js/navbar.js
@@ -5,28 +5,53 @@ window.addEventListener('DOMContentLoaded', function() {
 	
 	
 	const btnMenu = document.querySelector('.navbar-menu-btn');
-	const detailedMenu = document.querySelector('#navbar-detailed-menu');
+	
 	
 	// 메뉴버튼 누르면 토글될 수 있도록 함	
 	btnMenu.addEventListener('click', function() {
 		
+		const detailedMenu = document.querySelector('.navbar-detailed-menu');
+		
 		const menuHeight = detailedMenu.style.height;
+		const menuWidth = detailedMenu.style.width;
 		console.log(menuHeight);
 		console.log(detailedMenu.scrollHeight);
 		console.log(window.innerWidth);
-		if (menuHeight == '' || menuHeight == '0px') {
-			console.log('높이 현재 0')
-			detailedMenu.style.width = '85%';
-			detailedMenu.style.height = detailedMenu.scrollHeight + 'px';
-			detailedMenu.style.opacity = 1;
-		} else {
-			detailedMenu.style.height = 0;
-			detailedMenu.style.opacity = 0;
+		if (window.innerWidth > 1024) {	// 폭이 1024px초과할 때
+			if (menuHeight == '' || menuHeight == '0px') {	// 높이 0일 때 (접혀있을 때)
+				console.log('높이 현재 0');
+				detailedMenu.style.height = detailedMenu.scrollHeight + 'px';
+			} else {	// 메뉴바 펼쳐져 있을 때
+				
+				detailedMenu.style.height = 0;															
+	
+			}
+		} else {	// 폭이 1024px 이하일 때
+			
+			
+			
 		}
 
-			
 	});
 	
+	
+	window.addEventListener('resize', function() {		
+		
+		const detailedMenu = document.querySelector('.navbar-detailed-menu');
+
+		
+		const menuHeight = detailedMenu.style.height;
+		const menuWidth = detailedMenu.style.width;
+		
+		if (window.innerWidth < 1024) {
+			detailedMenu.style.width = '280px';
+			detailedMenu.style.height = '100vh';
+		} else {
+			detailedMenu.style.width = '85%';
+			detailedMenu.style.height = 'auto';
+		}
+		
+	})
 	
 	
 	
