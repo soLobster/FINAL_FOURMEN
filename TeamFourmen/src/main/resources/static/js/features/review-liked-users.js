7782cbe025cc4122b3accb663435645973bf19de
soLobster <dhwnd0526@gmail.com>
2024-03-04 20:49:27 +0900

    commit 20240304

--- TeamFourmen/src/main/resources/static/js/features/review-liked-users.js
+++ TeamFourmen/src/main/resources/static/js/features/review-liked-users.js
@@ -4,7 +4,6 @@
  *
  */
 
-
 document.addEventListener('DOMContentLoaded', async function () {
 
     // 좋아요 누른 유저의 div들을 찾음
@@ -27,34 +26,35 @@ document.addEventListener('DOMContentLoaded', async function () {
             button.classList.add('d-none');
         }
 
-        // 팔로우 여부를 가려서 팔로우를 한 유저라면 버튼을 언팔로우 버튼으로 바꾸고 'data-status'를 'following' 중으로 바꿈
-        // 버튼 역시 기본의 'btn-success' 에서 -> 'btn-danger'로 바꾸고 이름까지 언팔로우로 바꿈.
-        //
+        // 유저의 이메일을 통해 팔로우를 체크하고 팔로우 중 이라면 버튼의 색깔과 버튼 이름을 변경함.
         if(await checkFollowUser(userEmail) === true){
             button.classList.replace('notFollowed','isFollowed');
             button.classList.replace('btn-success', 'btn-danger');
             button.innerText = '언팔로우';
         }
 
-        //
-        if(button.querySelector('button').classList.contains('notFollowed')){
+        button.removeEventListener('click', await followUser);
+        button.removeEventListener('click', await unFollowUser);
+        /*
+        <button class ="mb-2 btn btn-success notFollowed"
+                                th:id = "${ 'user-' + user.getMember().getMemberId() }"
+                                th:email = "${user.getMember().getEmail()}">팔로우</button>
+        버튼은 이런 형식임.
+        그래서 클래스에 notFollowed가 있다면 팔로우 버튼을 누르게 됨
+        아닐시에는 언팔로우 버튼을 누르게 됨
+        TODO 근데 문제는 버튼에 이벤트 리스너가 한번 등록이 된 상태이기에 이벤트 리스너를 제거해줘야함 어디서 제거할지 모르겠음
+        getEventListener를 이용해서 이벤트 리스너가 등록 되고 있는지 확인하고 싶음
+         */
+
+        if(button.classList.contains('notFollowed')){
             button.addEventListener('click', async function(){
                 await followUser(userEmail , button);
             });
-            button.removeEventListener('click', followUser);
         } else {
             button.addEventListener('click', async function() {
                 await unFollowUser(userEmail, button);
             });
-            button.removeEventListener('click', unFollowUser);
         }
-        // status가 'following' 중이라면 팔로우 취소 버튼을 실행.
-        // if(status === 'following') {
-        //     button.addEventListener('click', async function(){
-        //         await unFollowUser(userEmail , button);
-        //     });
-        // }
-
     }
 
     async function checkFollowUser(userEmail) {
@@ -78,14 +78,11 @@ document.addEventListener('DOMContentLoaded', async function () {
             const response = await axios.post(`/api/follow/${userEmail}`);
             console.log(response.data);
 
-
             button.classList.replace('btn-success', 'btn-danger');
             button.classList.replace('notFollowed', "isFollowed");
             button.innerText = '언팔로우';
 
             alert('팔로우 되었습니다.');
-
-
         } catch (error) {
             console.log('팔로우 추가 오류', error);
         }
@@ -106,14 +103,11 @@ document.addEventListener('DOMContentLoaded', async function () {
             button.classList.replace('btn-danger' , 'btn-success');
             button.classList.replace('isFollowed', 'notFollowed');
             button.innerText = '팔로우';
+
             alert('언팔로우 되었습니다.');
-
-
         } catch (error) {
             console.log('팔로우 삭제 오류', error);
         }
-
-
     }
 
     async function checkCurrentUser() {
